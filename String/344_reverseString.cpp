5bf0be880c766ef0d9aef9f6fc4663db5ae06b29
Shantanu <shantanukundu2001@gmail.com>
2026-06-24 10:21:32 +0530

    Added solution to Leetcode problem 344 on June 24th '26

---
+++ String/344_reverseString.cpp
@@ -0,0 +1,50 @@
+// Author : Shantanu Kundu
+// Date: 24/06/2026
+
+/*
+========================================
+REVERSE STRING - LEETCODE 344
+========================================
+
+Description:
+Write a function that reverses a string. The input string is given as an array of characters s.
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+----------------------------------------
+Example 1
+----------------------------------------
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+ 
+----------------------------------------
+Example 2
+----------------------------------------
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+
+========================================
+SOLUTION 
+========================================
+
+Time Complexity  : O(n)
+Space Complexity : O(1)
+
+*/
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left=0, right=s.size()-1;
+        while(left<right){
+            char temp=s[left];
+            s[left]=s[right];
+            s[right]=temp;
+
+            left++;
+            right--;
+        }
+    }
+};
